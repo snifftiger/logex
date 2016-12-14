4069546aa36fd3fb977af8602e4bbe5224b20514
lidongye_vd <lidongye_vd@baidu.com>
2016-12-15 00:25:24 +0800

    golang log

---
+++ goid.go
@@ -0,0 +1,15 @@
+// -*- coding:utf-8; indent-tabs-mode:nil; -*-
+
+// Copyright 2014, Wu Xi. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// This file defines Goid() function, which is used to get the id of current
+// goroutine. More details about this function are availeble in the runtime.c
+// file of golang source code.
+
+package logex
+
+func goid() int32 {
+    return 0
+}
